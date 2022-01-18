e2d343282eeb603ebe115503ee60757772568825
firanlucianmadalin <firanlucian137@gmail.com>
2022-01-18 21:03:52 +0200

    curs 1

---
+++ hello/hello.js
@@ -0,0 +1,12 @@
+//ruleaza tellMe dupa 3000 ms(3 secunde)
+setTimeout(tellMe, 3000)
+//creeaza o noua instructiune (functie) numita tellMe
+function tellMe() {
+    alert("Hello world!")
+    alert("Welcome to JavaScript!")
+}
+/*
+comentariu pe
+mai multe linii
+*/
+//comentariu pentru o singura linie
