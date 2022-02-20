71037669695308c7418c1b8c73e7eb55f8aab20d
firanlucianmadalin <95710495+firanlucianmadalin@users.noreply.github.com>
2022-02-20 09:33:33 +0200

    js-2022

---
+++ decizii/if.js
@@ -0,0 +1,28 @@
+
+var  isRaining = adevarat
+var isSunny = adevarat
+var isSnowing = fals
+
+// if simplu
+{daca (ploua) 
+
+}
+
+// daca ai altceva:
+{daca (ploua)
+
+} altfel 
+
+
+
+// if cu mai multe ramuri:
+daca (ploua) 
+    document. scrie ('Nu uita de umbrela')
+    document. scrie ('Si nu uita de pelerina')
+  if ('este insorit') 
+    document. scrie ('Mancam o pizza la terasa')
+ else if ('este zapada') 
+    document. scrie ('Facem un om de zapada')
+ otherwise 
+    document. scrie ('Stai acasa')
+    
