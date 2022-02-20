a5fe9043cc078dae4fa9b4061a981aeb2ec00ac8
firanlucianmadalin <95710495+firanlucianmadalin@users.noreply.github.com>
2022-02-20 10:43:16 +0200

    js-2022

--- memory/memory.js
+++ memory/memory.js
@@ -1,72 +1,72 @@
-let  animalele  =  [
-    'urs' ,
-    'corgi' ,
-    "crab" ,
-    'pește' ,
-    'panda' ,
-    'papagal' ,
-    "porc" ,
-    'iepure' ,
-    'rinocer' ,
-    'rechin' ,
-    'melc',
-    'broasca testoasa',
+
+let animals = [
+    'bear',
+    'corgi',
+    'crab',
+    'fish',
+    'panda',
+    'parrot',
+    'pig',
+    'rabbit',
+    'rhinoceros',
+    'shark',
+    'snail',
+    'turtle'
 ]
-let  animalPairs  =  [ ... animale , ... animale ]
-let  cards  =  amestecați ( animalPairs )
-let  activeCard  =  null
-let  waitingToHideCards  =  false
+let animalPairs = [...animals, ...animals]
+let cards = shuffle(animalPairs)
+let activeCard = null
+let waitingToHideCards = false
 
-function  shuffle ( array )  {
-      returnare . sortare ( ( )  => 0,5  -  Matematică  ( ) )
+function shuffle(array) {
+    return array.sort(() => 0.5 - Math.random())
 }
 
-function  displayAllCards ( )  {
-    carduri . pentru  ( displayCard )
+function displayAllCards() {
+    cards.forEach(displayCard)
 }
 
-function  displayCard ( card )  {
-    let  div  =  document . createElement ( 'div' )
-    div . classList . adauga ( 'carte' )
-    let  img  =  document . createElement ( 'img' )
-    img . src  =  `https://raw.githubusercontent.com/miezuit/html-css-python/main/js/memory/img/icons8- ${ card } .png`
-    div . appendChild ( imagine )
-    document . querySelector ( '#game' ) . appendChild ( div )
-    div . addEventListener ( 'click' ,  showCard )
+function displayCard(card) {
+    let div = document.createElement('div')
+    div.classList.add('card')
+    let img = document.createElement('img')
+    img.src = `https://raw.githubusercontent.com/miezuit/html-css-python/main/js/memory/img/icons8-${card}.png`
+    div.appendChild(img)
+    document.querySelector('#game').appendChild(div)
+    div.addEventListener('click', showCard)
 }
 
-function  showCard ( eveniment )  {
-    let  card  =  eveniment . ţintă
+function showCard(event) {
+    let card = event.target
 
     // cazul in care nu este niciun card activ
-    if ( ! activeCard ){ 
-        activeCard  =  card
-        card . querySelector ( 'img' ) . stil . vizibilitate  =  'vizibil'
-    }  else  if  ( ! waitingToHideCards )  {
-        card . querySelector ( 'img' ) . stil . vizibilitate  =  'vizibil'
-        if ( cardsAreTheSame ( activeCard ,  card ) )  {
-            // cardurile sunt aceleași, păstrați vizibilitatea și eliminați ascultătorii
-            ActiveCard . removeEventListener ( 'click' ,  showCard )
-            card . removeEventListener ( 'click' ,  showCard )
-            activeCard  =  nul
-        }  else  {
-            // cărțile nu sunt la fel
-            // ascunde cărțile după 2 secunde
-            waitingToHideCards  =  adevărat
-            setTimeout ( ( )  =>  hideCards ( [ activeCard ,  card ] ) ,  2000 )
+    if (!activeCard) {
+        activeCard = card
+        card.querySelector('img').style.visibility = 'visible'
+    } else if (!waitingToHideCards) {
+        card.querySelector('img').style.visibility = 'visible'
+        if(cardsAreTheSame(activeCard, card)) {
+            // cards are the same, keep the visible and remove listeners
+            activeCard.removeEventListener('click', showCard)
+            card.removeEventListener('click', showCard)
+            activeCard = null
+        } else {
+            // cards are not the same
+            // hide the cards after 2 seconds
+            waitingToHideCards = true
+            setTimeout(() => hideCards([activeCard, card]), 2000)
         }
     }
 }
 
-   funcțiiAreTheSame ( card1 ,  card2 ) 
- 
-    return  card1 . querySelector ( 'img' ) . src  ==  card2 . querySelector ( 'img' ) . src
- 
-
-function  hideCards ( carduri )  {
-    carduri . forEach ( card  =>  card . querySelector ( 'img' ) . style . visibility  =  'hidden' )
-    activeCard  =  nul
-    waitingToHideCards  =  fals
+function cardsAreTheSame(card1, card2) {
+    return card1.querySelector('img').src == card2.querySelector('img').src
 }
 
-displayAllCards ( )
+function hideCards(cards) {
+    cards.forEach(card => card.querySelector('img').style.visibility = 'hidden')
+    activeCard = null
+    waitingToHideCards = false
+}
+
+displayAllCards()
